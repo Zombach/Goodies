ecfa872afb327328b4cecc6f32a7976d52957406
ZLoo <kozlov.s.v.1992@gmail.com>
2023-07-26 15:37:05 +0300

    Create Program.cs

---
+++ VS2022/Insight.Database/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Insight.Database;
+using Microsoft.Data.SqlClient;
+
+using SqlConnection connection = new(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=master;");
+Insight.Database.Providers.MsSqlClient.SqlInsightDbProvider.RegisterProvider();
+
+Beer beer = new() { ID=1, Type = "ipa", Description = "Sly Fox 113" };
+IBeerRepositoryDb repo = connection.As<IBeerRepositoryDb>();
+
+repo.InsertBeer(beer);
+IList<Beer> beerList = repo.GetBeerByType("ipa");
+repo.InsertBeerTable(beerList);
+
+Console.ReadLine();

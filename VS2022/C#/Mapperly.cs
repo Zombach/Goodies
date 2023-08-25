193868972eb34318ddc07eab753b15789e702f0f
ZLoo <kozlov.s.v.1992@gmail.com>
2023-08-25 15:40:23 +0300

    Update Mapperly.cs

--- VS2022/C#/Mapperly.cs
+++ VS2022/C#/Mapperly.cs
@@ -6,8 +6,16 @@ public class CarShortModel { }
 public class CarDTO { }
 public class CarShortDTO { }
 
+//Optional for use DI
+public interface ICarMapper
+{
+    public CarDTO CarModelToCarDTO(CarModel carModel);
+    public CarDTO CarModelToCarShortDTO(CarModel carModel);
+    public CarShortDTO CarShortModelToCarShortDTO(CarShortModel carModel);
+}
+
 [Mapper]
-public partial class CarMapper
+public partial class CarMapper: ICarMapper
 {
     public partial CarDTO CarModelToCarDTO(CarModel carModel);
     public partial CarDTO CarModelToCarShortDTO(CarModel carModel);
@@ -20,4 +28,25 @@ public static partial class CarMapperStatic
     public static partial CarDTO CarModelToCarDTO(CarModel  carModel);
     public static partial CarDTO CarModelToCarShortDTO(CarModel  carModel);
     public static partial CarShortDTO CarShortModelToCarShortDTO(CarShortModel carModel);
+}
+
+public class MapUseDI
+{
+    private ICarMapper _mapper;
+    public UseVar_DI(ICarMapper mapper) => _mapper = mapper;
+    public CarDTO Use(CarModel model) => _mapper.CarModelToCarDTO(model);
+}
+
+public class MapUseInstance 
+{
+    public CarDTO Map(CarModel model)
+    {
+        CarMapper mapper = new();
+        mapper.CarModelToCarDTO(model);
+    }
+}
+
+public class MapUseStatic
+{
+    public CarDTO Map(CarModel model) => CarMapperStatic(model);
 }
